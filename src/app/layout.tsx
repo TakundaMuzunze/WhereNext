842f50033ddeaa5ac177ccbf24741a58fdd8beb0
Takunda Muzunze <takmuzunze@gmail.com>
2026-07-21 19:48:40 +0100

    feat: initiliase page theme for refreshes and page visits

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,21 @@ import { Footer } from "@/widgets/footer";
 import { Header } from "@/widgets/header";
 import "./globals.css";
 
+const initiliaseTheme = `
+  (() => {
+    const savedTheme = localStorage.getItem("theme");
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+
+    const useDarkTheme =
+      savedTheme === "dark" ||
+      (!savedTheme && prefersDark);
+
+    document.documentElement.classList.toggle("dark", useDarkTheme);
+  })();
+`;
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -9,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: initiliaseTheme }} />
+      </head>
       <body>
         <Header />
         <div>{children}</div>
